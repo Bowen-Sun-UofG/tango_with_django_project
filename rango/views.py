584500b93db5f9ed1c8578b3f8b688a1ba6b0edc
Bowen-Sun-UofG <2962124S@student.gla.ac.uk>
2025-02-17 10:44:45 +0000

    Updated Django project

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    category_list = Category.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list}
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name):
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        category = None
+        pages = None
+
+    context_dict = {'category': category, 'pages': pages}
+    return render(request, 'rango/category.html', context=context_dict)
+
