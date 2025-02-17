7675938862821cc3d6c86c7525a9e10fe71dfb50
Bowen-Sun-UofG <2962124S@student.gla.ac.uk>
2025-02-17 16:35:46 +0000

    Chapter 5 Update

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')  # ✅ 添加这个
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)  # ✅ 这样 Page 在 Admin 里会显示正确的列
